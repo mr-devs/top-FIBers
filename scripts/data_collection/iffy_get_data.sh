c133d0642f057eef2a1a2a434f835f026e5d4d33
Nick Liu <nick.changliu@gmail.com>
2023-02-01 20:10:48 -0500

    Update iffy_get_data.sh
    update the script to work at the repo root, changed the naming format for new iffy list files downloaded, and added docstring.

--- scripts/data_collection/iffy_get_data.sh
+++ scripts/data_collection/iffy_get_data.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
+
+# Purpose:
+#   This is a script that downloads the latest iffy list, then retrieve and save relevant raw twitter data from the past month.
+#   The process goes between different servers(Lenny, Lisa, Moe) for convinience and load balancing.
+#   
+# Inputs:
+#   None
+#
+# Configs:
+#   fiber_home :  topfibers project folder
+#   tavern_job :  moe's tavern is used for raw twitter data retrival, files would first need to be output to this shared path on Lisa, 
+#                 so that files could later be transfered to Lenny.
+#   KEY        :  the ssh key for Moe as appuser
+#
+# Output:
+#   A folder named YYYY-MM containing data retrival results for the past month
+#
+# How to call:
+#   ```
+#   cd /home/data/apps/topfibers/repo
+#   nohup ./scripts/data_collection/iffy_get_data.sh > ./logs/iffy_get_data.out 2>&1 &
+#   ```
+#
+#   The above will output results to ${fiber_home}moe_twitter_data/YYYY-MM and save logs within /var/log/iffy_get_data.out
+#
+# Author: Nick Liu
+
 year_month=$(date --date='last month' '+%Y-%m')
 end_of_last_month=$(date -d "$(date +%Y-%m-01) -1 day" +%Y-%m-%d)
+today="$(date +%Y-%m-%d)__"
 
 fiber_home="/home/data/apps/topfibers/"
 tavern_job="osome_swap/moe/jobs/top_fibers_data/"
 KEY=${HOME}/.ssh/id_rsa_moe
 
-#check variables
-#echo "Start: " ${year_month}-01  "End: " ${end_of_last_month} "updating iffy"
-#echo "Fibers Home: " ${fiber_home}  
-#echo "Tavern Dir: " ${tavern_job}  
-#echo "Moe Key: "${KEY}
-#echo "rsync -at ${fiber_home}iffy_list truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}/"
-#echo "rsync -rt truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}${year_month}/ ${fiber_home}moe_twitter_data/"
+# go to repo root
+cd ${fiber_home}repo
 
 # get latest iffy list
-${fiber_home}repo/environments/env_code/bin/python /u/truthy/cronjobs/update_latestmonth_topfibers/iffy_update.py "${fiber_home}iffy_list"
+environments/env_code/bin/python scripts/data_collection/iffy_update.py -f "data/iffy_files/${today}iffy_list.txt"
 
 # copy to Lisa-Moe shared drive
-rsync -at ${fiber_home}iffy_list truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}/
+rsync -at data/iffy_files/${today}iffy_list.txt truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}${today}iffy_list.txt
 
 # create a tavern job
-ssh -i ${KEY} appuser@moe-ln01.soic.indiana.edu "/home/appuser/truthy-cmd.sh get-tweets-with-meme -memes "/mnt/${tavern_job}iffy_list" -tstart "${year_month}-01" -tend "${end_of_last_month}" -tid false -ntweets 1000000 -outdir /mnt/${tavern_job}${year_month}/ -torf false"
+ssh -i ${KEY} appuser@moe-ln01.soic.indiana.edu "/home/appuser/truthy-cmd.sh get-tweets-with-meme -memes "/mnt/${tavern_job}${today}iffy_list.txt" -tstart "${year_month}-01" -tend "${end_of_last_month}" -tid false -ntweets 1000000 -outdir /mnt/${tavern_job}${year_month}/ -torf false"
 
 # copy results to Lenny:topfibers 
-rsync -rt truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}${year_month}/ ${fiber_home}moe_twitter_data/
+rsync -rt truthy@lisa.luddy.indiana.edu:/home/data/${tavern_job}${year_month} ${fiber_home}moe_twitter_data/
