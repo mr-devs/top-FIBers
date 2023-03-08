1f90e603c1e1ac4cb357f0ea5c1d26b3cd63d9a6
Truthy <truthyatindiana@gmail.com>
2023-03-08 15:38:32 -0500

    add code review changes

--- data-loader/server.py
+++ data-loader/server.py
@@ -1,15 +1,30 @@
+"""
+Purpose:
+    This script fetch the current month data and feed the data in to database
+Inputs:
+    *No inputs to the function
+    *If you need to run for the past months which is missed. use the LOAD_PAST_MONTH_DATA array
+    *This script will grab the current year_month and execute it.
+Output:
+    *No output available, this will add data to database.
+Author: Pasan Kamburugamuwa
+"""
+
 from flask import Flask
 from app import controller
 import re
 import os
 import datetime
 
+#get the absolute path of the facebook dir
 facebook_data_root = os.path.abspath(os.path.join(os.getcwd(), "../data/derived/fib_results/facebook/"))
+#get the abosolute path of the twitter dir
 twitter_data_root = os.path.abspath(os.path.join(os.getcwd(),"../data/derived/fib_results/twitter/"))
 PLATFORMS = ["Facebook", "Twitter"]
+#to load the past months data(2022_01, 2022_02, 2022_04 ..) use this array
 LOAD_PAST_MONTH_DATA = ['2022_01']
 
-def load_current_data():
+def load_current_month_data():
     """
     Load each platform and read the files.
     """
@@ -18,16 +33,21 @@ def load_current_data():
             read_dir = facebook_data_root
         else:
             read_dir = twitter_data_root
-        all_year_month = get_available_year_month(read_dir)
-        current_year_month = datetime.datetime.now().strftime("%Y_%m")
-        selected_month = list(filter(lambda x: current_year_month in x, all_year_month))
-        if len(selected_month) == 1:
-            controller.read_files(read_dir, platform, selected_month[0])
-        else:
-            raise Exception("Error in finding data on", current_year_month)
-            logger.error("Error in finding data on {}", current_year_month)
+        try:
+           #get the current year_month
+           current_year_month = datetime.datetime.now().strftime("%Y_%m")
+           current_year_month_dir = os.path.join(read_dir, current_year_month)
+           try:
+               #call the read_file func in controller to read the file
+               controller.read_files(read_dir, platform, selected_month[0])
+           except Exception as err:
+               traceback.print_tb(err.__traceback__)
+               logger.error("Error in adding data to database")
+        except Exception as err:
+           traceback.print_tb(err.__traceback__)
+           logger.error("Error in finding data on {}",current_year_month)
 
-def load_past_data():
+def load_past_months_data():
     """
     Load each platform and read the files.
     """
@@ -39,18 +59,6 @@ def load_past_data():
                 read_dir = twitter_data_root
             controller.read_files(read_dir, platform, selected_month)
 
-def get_available_year_month(root_dir):
-    """
-    Get available year and month
-    """
-    year_months = []
-    for root_folder in os.listdir(root_dir):
-        match = re.search(r'\d{4}_\d{2}', root_folder)
-        if match is None:
-            continue
-        year_months.append(match.group())
-    return year_months
-
 if __name__ == '__main__':
-    load_past_data()
+    load_past_months_data()
 
